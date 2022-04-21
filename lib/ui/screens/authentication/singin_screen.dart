e621670996e9bd6be375b7eb5225108fd07e8f3c
nikhilsn <nikhilsoni7991@gmail.com>
2022-04-21 13:57:38 +0530

    login screen ui fixes

--- lib/ui/screens/authentication/singin_screen.dart
+++ lib/ui/screens/authentication/singin_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:roll_a_dice/resources/constants.dart';
 import 'package:roll_a_dice/services/authentication/google_authentication.dart';
 
 class SinginScreen extends StatefulWidget {
@@ -9,17 +12,48 @@ class SinginScreen extends StatefulWidget {
 }
 
 class _SinginScreenState extends State<SinginScreen> {
-
-  login() async{
+  _login() async {
     await Future.delayed(Duration(seconds: 3));
     LoginWithGoogle().signIn();
-
   }
+
+  Widget _imageWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/dice1.png',
+          scale: 0.9,
+        ),
+        Image.asset(
+          'assets/dice3.png',
+          scale: 0.9,
+        ),
+        Image.asset(
+          'assets/dice5.png',
+          scale: 0.9,
+        ),
+        Image.asset(
+          'assets/dice6.png',
+          scale: 0.9,
+        ),
+      ],
+    );
+  }
+
+  Widget _signinWithGoogleButton() {
+    return SignInButton(
+      Buttons.Google,
+      onPressed: _login,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
-    login();
+    // login();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +65,29 @@ class _SinginScreenState extends State<SinginScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('data')
+                Expanded(
+                  flex: 2,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          Constants.appName,
+                          style: const TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        _imageWidget(),
+                      ],
+                    ),
+                  ),
+                ),
+                Center(
+                  child: _signinWithGoogleButton(),
+                ),
+                Expanded(child: Container())
               ],
             ),
           ),
